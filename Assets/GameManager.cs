276a2a9cfcf418803acce5597338de600749ffe0
JuliaSchneider01 <julia14.schneider@t-online.de>
2024-05-07 21:19:20 +0200

    add basic template

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,20 +4,67 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro; 
 
+class StoryBlock {
+    public string story;
+    public string optionA_Text;
+    public string optionB_Text;
+    public string optionC_Text;
+    public StoryBlock optionA_Block;
+    public StoryBlock optionB_Block;
+    public StoryBlock optionC_Block;
+
+    public StoryBlock(string story, string optionA_Text = "", string optionB_Text = "", string optionC_Text ="", 
+                        StoryBlock optionA_Block =null, StoryBlock optionB_Block = null, StoryBlock optionC_Block = null){
+        this.story = story;
+        this.optionA_Text = optionA_Text;
+        this.optionB_Text = optionB_Text;
+        this.optionC_Text = optionC_Text;
+        this.optionA_Block = optionA_Block;
+        this.optionB_Block = optionB_Block;
+        this.optionC_Block = optionC_Block;
+    }
+}
+
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI SzeneText;
     public Button Option_A;
     public Button Option_B;
     public Button Option_C;
+    StoryBlock currentBlock;
     
-    // Start is called before the first frame update
+    static StoryBlock block3 = new StoryBlock("Freut mich!", "", "", "");
+    static StoryBlock block2 = new StoryBlock("Oh", "", "", "");
+    static StoryBlock block1 = new StoryBlock("Wie gehts?", "Gut", "schlecht", "", block3, block2);
+
+   
+
     void Start()
     {
-        SzeneText.text = "Welcome";
-        Option_A.GetComponentInChildren<TMP_Text>().text = "option1";
-        Option_B.GetComponentInChildren<TMP_Text>().text = "option2";
-        Option_C.GetComponentInChildren<TMP_Text>().text = "option3";
+       DisplayBlock(block1);
     }
-}
 
+    void DisplayBlock(StoryBlock block){
+        SzeneText.text = block.story;
+        Option_A.GetComponentInChildren<TMP_Text>().text = block.optionA_Text;
+        Option_B.GetComponentInChildren<TMP_Text>().text = block.optionB_Text;
+        Option_C.GetComponentInChildren<TMP_Text>().text = block.optionC_Text;
+
+        currentBlock = block;
+
+    }
+
+    public void ButtonA_clicked(){
+        DisplayBlock(currentBlock.optionA_Block);
+    }
+    
+    public void ButtonB_clicked(){
+        DisplayBlock(currentBlock.optionB_Block);
+    }
+    
+    public void ButtonC_clicked(){
+        DisplayBlock(currentBlock.optionC_Block);
+        
+    }
+
+}
